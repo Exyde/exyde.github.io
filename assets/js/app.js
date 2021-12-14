89a7ba0458b214890fe4baeead049fd90239e1d2
Exyde <54008677+Exyde@users.noreply.github.com>
2021-12-14 11:24:26 +0100

    Gallery & Creative coding.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,35 @@
 // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
-
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
+if (scrollUp){
+  scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
   });
-});
+}
 
 //Toggle the menu items.
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
-  });
+if (burger){
+  burger.addEventListener("click", () => {
+      ul.classList.toggle("show");
+    });
+}
 
 // Close hamburger menu when a link is clicked
-
-// Select nav links
 const menuItem = document.querySelectorAll(".MenuItem");
-
-menuItem.forEach((item) =>
+if (menuItem){
+  menuItem.forEach((item) =>
   item.addEventListener("click", () => {
     ul.classList.remove("show");
   })
 );
 
-menuItem.forEach((item) =>
-  item.addEventListener("mouseOver", () => {
-    item.style.color = "purple";
-  })
-);
+}
 
-//Nav hovering
 
 
 
